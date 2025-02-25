95a4eefb078457fe187767af4edc5aa298d0c23d
vikaradnaeva <vikarad2006@gmail.com>
2025-02-25 23:54:21 +0700

    Add files via upload

---
+++ seminar/seminar/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace seminar
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int res = SumOfDigitsOfNumbers(1, 100);
+            Console.WriteLine(res);
+        }
+        // метод, который возвращает сумму четных чисел от 1 до 100
+        static int SumOfDigitsOfNumbers(int left, int right)
+        {
+            int sum = 0;
+            for (int number = left; number <= right; number++)
+            {
+                int tmp = Math.Abs(number);
+                while (tmp > 0)
+                {
+                    if (tmp % 2 == 0)
+                        sum += tmp % 10;
+                    tmp /= 10;
+                }
+            }
+            return sum;
+        }
+    }
+
+}
